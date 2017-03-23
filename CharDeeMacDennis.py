9e1dce7bb30f09a8aa33fa5eb4ec9c1837ff20bc
VanguardFed <federationsrising@gmail.com>
2017-03-23 16:47:19 -0400

    Added file

---
+++ CharDeeMacDennis.py
@@ -0,0 +1,15 @@
+#CharDee MacDennis
+
+class Card:
+    def __init__(self):
+        level=0
+        category="null"
+        content="null"
+    def display(self):
+        print level
+        print category
+        print content
+    
+
+test=Card()
+test.display()
