403fdfb90d94bdbbafb32ebecf5c816db9ce6d1b
VanguardFed <federationsrising@gmail.com>
2017-03-24 09:40:15 -0400

    Edited Card.display()

--- CharDeeMacDennis.py
+++ CharDeeMacDennis.py
@@ -1,15 +1,26 @@
 #CharDee MacDennis
 
 class Card:
-    def __init__(self):
-        level=0
-        category="null"
-        content="null"
+
+    #sets up a card with given variables
+    def __init__(self,level,category,content):
+        self.lev=level
+        self.cat=category
+        self.con=content
+
+    #displays a card
     def display(self):
-        print level
-        print category
-        print content
+        blankline="|                                                    |||"
+        characters=len(blankline)
+        print "_"*characters
+        print "  /"+("_"*(characters-4))+"/"
+        print " /____________________________________________________/|"
+        print blankline
+        print "|                    Level",self.lev,"                        |||"
+        print self.lev
+        print self.cat
+        print self.con
     
 
-test=Card()
+test=Card(0,"null","null")
 test.display()
