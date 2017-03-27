df06c22bc80f6780dea22f2b2d5a5044b02541a5
VanguardFed <federationsrising@gmail.com>
2017-03-27 12:31:06 -0400

    Changed Card.display() to barebones version, added to-do lsit

--- CharDeeMacDennis.py
+++ CharDeeMacDennis.py
@@ -1,26 +1,44 @@
-#CharDee MacDennis
+#------------------------------------------------------------
+#
+# CharDee MacDennis
+# 
+# Authors: Trent Thompson, Nicole Riker, Collin Tidwell
+# 
+# Started: 03-27-2017
+#
+#------------------------------------------------------------
+
+# TO-DO:
+#   [X] Card class
+#   [X] Barebones card display function
+#   [ ] Card storage system
+#   [ ] Card submission function (user input)
+#   [ ] Team class
+#   [ ] Team creation function (user input)
 
 class Card:
 
     #sets up a card with given variables
-    def __init__(self,level,category,content):
+    def __init__(self,author,level,category,content,tags):
+        self.aut=author
         self.lev=level
         self.cat=category
         self.con=content
+        self.tag=tags
 
     #displays a card
     def display(self):
-        blankline="|                                                    |||"
-        characters=len(blankline)
-        print "_"*characters
-        print "  /"+("_"*(characters-4))+"/"
-        print " /____________________________________________________/|"
-        print blankline
-        print "|                    Level",self.lev,"                        |||"
-        print self.lev
-        print self.cat
-        print self.con
+##        blankline="|                                                    |||"
+##        characters=len(blankline)
+##        print "   "+"_"*(characters-3)
+##        print "  /"+("_"*(characters-4))+"/"
+##        print " /____________________________________________________/|"
+##        print blankline
+##        print "|                    Level",self.lev,"                        |||"
+        print "Level:",self.lev
+        print "Category:",self.cat
+        print "Content:",self.con
     
 
-test=Card(0,"null","null")
+test=Card("dev",0,"test","null","N/A")
 test.display()
